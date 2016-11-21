b572856ffbe1b164ff62c23a338bba5ad905b5d3
System Administrator <root@MarsdeMacBook-Pro.local>
2016-11-21 16:53:52 +0800

    upgrade

--- splunk.py
+++ splunk.py
@@ -1,47 +1,38 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import urllib
-import urllib2
-import json
-import sys
-import gzip
-import csv
-import simplejson
+#Auther:xwjr.com
+import urllib2,sys,gzip,csv,json
 
-CorpID = "wx5d8053"
-Secret = "FOPk4Inz6-_NjeZ9gHN1zJG"
-
+CorpID = "wx5d814254ec559053"
+Secret = "FOPk4InFkBg1ju6_MwYqKrLtXaAQbR7GD_8QgtpaNlayvHFdz6-_NjeZ9gHN1zJG"
+ToTag = "9"
+AgentID = "1"
 get_AccessToken = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=%s&corpsecret=%s" % (CorpID, Secret)
 
 req = urllib2.Request(get_AccessToken)
+response_data = urllib2.urlopen(req)
+res = eval(response_data.read())
+Token=res["access_token"]
 
-res_data = urllib2.urlopen(req)
-res = eval(res_data.read())
 
-token=res["access_token"]
 
-gz = sys.argv[8]
+GzipFile = sys.argv[8]
+CsvFile = gzip.open(GzipFile)
+CsvRead = csv.reader(CsvFile)
+for row in CsvRead:
+    SplunkLogInfo = row[2]
 
-f = gzip.open(gz)
-read = csv.reader(f)
-for row in read:
-    print row[2]
-    data = row[2]
-
-foo = sys.argv[3]
-print foo
-
-def send_message(msg):
-    url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s" % token
-    Header = "Splunk提醒！！"
-    values = {"totag": "9","msgtype": "text","agentid": 1,"text": {"content": Header + '\n' + data},"safe":"0"}
-    jdata = simplejson.dumps(values, ensure_ascii=False).encode('utf-8') 
+def SendMessage(SplunkLogInfo,ToTag,AgentID):
+    url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s" % Token
+    Header = "splunk提醒！！"
+    values = {"totag": ToTag,"msgtype": "text","agentid": AgentID,"text": {"content": Header + '\n' + SplunkLogInfo},"safe":"0"}
+    jdata = json.dumps(values, ensure_ascii=False)
     req = urllib2.Request(url, jdata)
     response = urllib2.urlopen(req) 
     return response.read() 
 
-resp = send_message(foo)
-print resp
+Response = SendMessage(SplunkLogInfo,ToTag,AgentID)
+print Response
 
 
 
